36b523f21e467717ebf6090fcaf3eb05589dac71
chenhaoheng <3116004496@mail2.gdut.deu.cn>
2022-03-11 18:40:00 +0800

    +: 1

---
+++ LeetCode/src/main/java/com/lc/array/InsertIntoSortedIntervals.java
@@ -0,0 +1,67 @@
+package com.lc.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author chanhaoheng
+ * @date 2022/3/11
+ */
+public class InsertIntoSortedIntervals {
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        if (intervals == null || intervals.length == 0) {
+            return new int[][]{newInterval};
+        }
+
+        List<int[]> result = new ArrayList<>();
+
+        int curIndex = 0;
+        // 将不需要合并的先放result中，curIndex就是需要合并的interval
+        for (; curIndex < intervals.length; curIndex++) {
+            int[] curInterval = intervals[curIndex];
+            if (curInterval[1] < newInterval[0]) {
+                result.add(curInterval);
+            } else {
+                if (newInterval[1] >= curInterval[0]) {
+                    // 插在这个internal里面，再将这个internal跟后面的合并
+                    curInterval[0] = Math.min(curInterval[0], newInterval[0]);
+                    curInterval[1] = Math.max(curInterval[1], newInterval[1]);
+                } else {
+                    result.add(newInterval);
+                }
+                result.add(curInterval);
+                break;
+            }
+        }
+
+        // lc56的解法，直接合并
+        int lastIntervalIndex = curIndex;
+        for (int i = curIndex + 1; i < intervals.length; i++) {
+            int[] lastInterval = result.get(lastIntervalIndex);
+            // 若上一个interval的有边界小于当前interval左边界，直接add
+            if (lastInterval[1] < intervals[i][0]) {
+                for (int j = i; j < intervals.length; j++) {
+                    result.add(intervals[j]);
+                }
+            } else {
+                lastInterval[1] = Math.max(lastInterval[1], intervals[i][1]);
+            }
+        }
+
+        return result.toArray(new int[0][0]);
+    }
+
+    public static void main(String[] args) {
+        InsertIntoSortedIntervals intervals = new InsertIntoSortedIntervals();
+        int[][] intervals1 = {new int[]{1, 2}, new int[]{3, 5}, new int[]{6, 7}, new int[]{8, 10}, new int[]{12, 16}};
+        System.out.println(Arrays.deepToString(intervals.insert(intervals1, new int[]{4, 8})));
+
+        int[][] intervals2 = {new int[]{9, 10}};
+        System.out.println(Arrays.deepToString(intervals.insert(intervals2, new int[]{4, 8})));
+
+        int[][] intervals3 = {};
+        System.out.println(Arrays.deepToString(intervals.insert(intervals3, new int[]{4, 8})));
+    }
+}
--- LeetCode/src/main/java/com/lc/array/MergeIntervals.java
+++ LeetCode/src/main/java/com/lc/array/MergeIntervals.java
@@ -14,9 +14,31 @@ public class MergeIntervals {
         // 先对数组排序
         Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
         
-        // 数组排序后
+        // 数组排序后，可以枚举出两两数组的分布情况
+        // 对于数组[a, b]和[m, n]
+        // 若m > n   不合并，直接开一格
+        // 其他情况，即便b > n或 b<= n  需要合并数组, 合并的数组的有边界都是取max(b, n)
         int[][] result = new int[intervals.length][2];
-        
-        return result;
+
+        // 每次合并都是针对result[lastIndex]合并, result最多intervals.length个
+        int lastIndex = -1;
+
+        for (int i = 0; i < intervals.length; i++) {
+            if (i == 0 || intervals[i][0] > result[lastIndex][1]) {
+                result[++lastIndex] = intervals[i];
+            } else {
+                result[lastIndex][1] = Math.max(result[lastIndex][1], intervals[i][1]);
+            }
+        }
+
+        return Arrays.copyOf(result, lastIndex + 1);
+    }
+
+
+    public static void main(String[] args) {
+        MergeIntervals mergeIntervals = new MergeIntervals();
+        int[][] intervals = {new int[]{1, 3}, new int[]{2, 6},
+                new int[]{8, 10}, new int[]{15, 18}};
+        System.out.println(Arrays.deepToString(mergeIntervals.merge(intervals)));
     }
 }
